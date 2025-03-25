8070367f8a9c0fe860c8ce0dfda31d636ad003e6
mixmaster1989 <igoritmagic@gmail.com>
2025-03-25 11:55:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const { Telegraf } = require("telegraf");
 const { Database } = require("sqlite3").verbose();
-
+const { Telegraf, session, Scenes } = require("telegraf");
 // Инициализация базы данных SQLite
 const db = new Database("database.sqlite");
 db.serialize(() => {
