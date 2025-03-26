05f4021124c0c05a0814e3c5673dc41d57962f46
mixmaster1989 <igoritmagic@gmail.com>
2025-03-26 20:07:54 +0300

    5345345345345345

--- bot.js
+++ bot.js
@@ -127,6 +127,9 @@ bot.action("article", async (ctx) => {
 });
 
 addMaterialScene.on("text", async (ctx) => {
+  if (!ctx.session) ctx.session = {}; // Инициализируем сессию, если она отсутствует
+  if (!ctx.session.material) ctx.session.material = {}; // Инициализируем объект material, если он отсутствует
+
   console.log("Получено сообщение:", ctx.message.text); // Логируем текст сообщения
   console.log("Сессия материала:", ctx.session.material); // Логируем объект ctx.session.material
 
