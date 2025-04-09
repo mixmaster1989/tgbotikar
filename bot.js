17eb60c3e8fb909595ba3292ba29395cd3476df2
mixmaster1989 <igoritmagic@gmail.com>
2025-04-09 14:00:21 +0300

    Ошибка TypeError: response.split is not a function указывает на то, что в функции parseAIResponse вы пытаетесь вызвать метод split на объекте, который не является строкой. Судя по логам, метод generate возвращает объект с полем text, содержащим текст ответа модели. Однако в коде вы передаёте весь объект response в функцию parseAIResponse, вместо того чтобы передать только поле response.text.

--- bot.js
+++ bot.js
@@ -885,7 +885,7 @@ D) [вариант]
             console.log(response);
 
             // Парсим вопросы из ответа
-            const questions = parseAIResponse(response);
+            const questions = parseAIResponse(response.text); // Передаём только текст ответа
             allQuestions.push(...questions);
         }
 
