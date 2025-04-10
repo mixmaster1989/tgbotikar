11e2bb5e4f6931e7038545d7438d01838f042a77
mixmaster1989 <igoritmagic@gmail.com>
2025-04-10 09:32:41 +0300

    тест вызова

--- gpt4all_test.js
+++ gpt4all_test.js
@@ -1,23 +1,3 @@
-const { loadModel } = require("gpt4all");
+const { GPT4All } = require("gpt4all");
 
-async function getGpt4All() {
-  try {
-    const model = await loadModel("mistral-7b-instruct-v0.1.Q4_0.gguf", {
-      device: "cpu",
-      nCtx: 2048,
-      verbose: true,
-    });
-
-    // Проверка успешной инициализации модели
-    if (!model) {
-      throw new Error("Модель не была инициализирована.");
-    }
-
-    return model;
-  } catch (err) {
-    console.error("Ошибка в getGpt4All функции:", err);
-    return null; // Возвращаем null в случае ошибки
-  }
-}
-
-module.exports = getGpt4All;
+console.log(typeof GPT4All);
