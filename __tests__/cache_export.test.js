d2882eab6f6bcac3457881222f6962445f80750d
mixmaster1989 <igoritmagic@gmail.com>
2025-04-22 11:32:00 +0300

    ребилд теста кеш+яд

--- __tests__/cache_export.test.js
+++ __tests__/cache_export.test.js
@@ -1,10 +1,16 @@
 const path = require("path");
 const fs = require("fs-extra");
 const { exportCacheToJsonFile } = require("../modules/cache_export");
-const { saveToCacheHistory } = require("../modules/cache");
+const { saveToCacheHistory, fuzzyFindInCache } = require("../modules/cache");
+const db = require('sqlite3').verbose();
+const database = new db.Database(path.join(__dirname, '..', 'database.sqlite'));
 
 describe("Cache export module", () => {
   const localPath = path.join(__dirname, "..", "cache", "dataset.test.json");
+
+  beforeEach((done) => {
+    database.run("DELETE FROM gpt_cache WHERE prompt = ?", [question], done);
+  });
 
   it("should export cache to JSON after saving new entry", (done) => {
     const question = "Тестовый вопрос для экспорта";
@@ -22,5 +28,25 @@ describe("Cache export module", () => {
       expect(found).toBeTruthy();
       done();
     });
+  });
+});
+
+describe('Cache module', () => {
+  const question = 'Тестовый вопрос ' + Date.now();
+  const answer = 'Тестовый ответ';
+
+  beforeEach((done) => {
+    database.run("DELETE FROM gpt_cache WHERE prompt = ?", [question], done);
+  });
+
+  it('should save and find answer in cache', (done) => {
+    saveToCacheHistory(question, answer);
+
+    setTimeout(() => {
+      fuzzyFindInCache(question, (err, found) => {
+        expect(found).toBe(answer);
+        done();
+      });
+    }, 200);
   });
 });
